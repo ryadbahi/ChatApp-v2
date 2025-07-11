d7cb4eced098a057d2d82aa7447fd0506ec6c97e
ryad <ryadbahi@gmail.com>
2025-07-12 00:46:39 +0100

    fix: Update deployment configuration and allowed origins for consistency

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -27,7 +27,7 @@ const allowedOrigins = [
   "https://chat-app-v2-eosin.vercel.app",
   "https://chat-app-v2-ryadbahis-projects.vercel.app",
   "https://chat-app-v2-git-main-ryadbahis-projects.vercel.app",
-  "https://ryadbahi.github.io", // GitHub Pages domain
+  "http://ryadbah.me/ChatApp-v2/", // GitHub Pages domain
 ];
 const io = new Server(server, {
   cors: {
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/ChatApp-v2/' : '/',
+  base: process.env.NODE_ENV === "production" ? "/ChatApp-v2/" : "/",
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
