bd3ba7524da27fee372ac84781c1d5c0213e0ea8
ryadbahi <ryadbahi@users.noreply.github.com>
2025-07-12 01:07:56 +0000

    deploy: 23f31adb441c3c863cb096754190229cab12b107

--- assets/LoginRegister-BJ-3zwqc.js
+++
@@ -1,8 +0,0 @@
-import{j as e}from"./ui-vendor-BEMU751j.js";import{r as s}from"./react-vendor-oin3n_bH.js";import"./auth-CMvCJYTb.js";import{A as o}from"./chat-DTYdApTY.js";import"./social-Dqso1mru.js";import"./socket-vendor-BLRFddlS.js";import"./editor-vendor-DpIsqUJN.js";const x=()=>{const[r,t]=s.useState(!1);return e.jsx("div",{className:"min-h-screen flex items-center justify-center",children:e.jsx("div",{className:`
-        w-full max-w-md
-        h-[450px]
-        bg-white/10 backdrop-blur-md
-        border border-white/20
-        rounded-2xl shadow-2xl
-        p-8
-      `,children:e.jsx(o,{isRegister:r,onToggle:()=>t(!r)})})})};export{x as default};
